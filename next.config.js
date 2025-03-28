2b7c7082c6fa9e17bfabb5504e2c2ba5d1dad7f5
Francisco Javier Castillo Barrios <javier_castillo_15@hotmail.es>
2025-03-28 03:00:50 -0500

    update changes to deploid

--- next.config.js
+++ next.config.js
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
+    swcMinify: false, // Deshabilita la minificación con SWC
     images: {
-        domains: ['open.cruip.com','ucarecdn.com', 'www.svgrepo.com','images.unsplash.com', 'res.cloudinary.com']
+      domains: [
+        'open.cruip.com',
+        'ucarecdn.com',
+        'www.svgrepo.com',
+        'images.unsplash.com',
+        'res.cloudinary.com'
+      ],
+      unoptimized: true
     },
     webpack(config) {
-        config
-            .module
-            .rules
-            .push({test: /\.svg$/, use: ["@svgr/webpack"]});
-
-        return config;
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      });
+      return config;
     }
-}
-
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig;
+  
